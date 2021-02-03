333781af0bfd8273f6d0970ad900c1de126b1b04
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-02-03 09:50:27 +0100

    Added logging of which url is beeing requests

--- App/index.js
+++ App/index.js
@@ -6,10 +6,22 @@
 
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // The server should respond to requests with a string
 const server = http.createServer((req, res) => {
+  // Get url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send response
   res.end("Hello World\n");
+
+  // Log the request path
+  console.log("Request recieved on path: " + trimmedPath);
 });
 
 // Start server, listen on port 3000
