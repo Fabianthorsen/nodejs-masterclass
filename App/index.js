f57271ef64627138689dd49bd5a9169b0ee8bdce
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-02-03 10:21:57 +0100

    Added payload handling

--- App/index.js
+++ App/index.js
@@ -5,8 +5,9 @@
  */
 
 // Dependencies
-const http = require("http");
-const url = require("url");
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to requests with a string
 const server = http.createServer((req, res) => {
@@ -15,7 +16,7 @@ const server = http.createServer((req, res) => {
 
   // Get the path
   const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
   // Get the querystring as an object
   const queryStringObject = parsedUrl.query;
@@ -26,14 +27,25 @@ const server = http.createServer((req, res) => {
   // Get headers as object
   const headers = req.headers;
 
-  // Send response
-  res.end("Hello World\n");
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  // Log the request path
-  console.log("Request recieved with these headers: ", headers);
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Send response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request recieved with this payload: ', buffer);
+  });
 });
 
 // Start server, listen on port 3000
 server.listen(3000, () => {
-  console.log("The server is listening on port 3000");
+  console.log('The server is listening on port 3000');
 });
