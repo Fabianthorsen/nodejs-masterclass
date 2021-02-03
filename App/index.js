8f574f2f054753761c109248eb8fbb37fbdee8ee
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-02-03 09:29:38 +0100

    Set up server and server to listen on localhost

--- App/index.js
+++ App/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+/*
+ *
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+const http = require("http");
+
+// The server should respond to requests with a string
+const server = http.createServer((req, res) => {
+  res.end("Hello World\n");
+});
+
+// Start server, listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000");
+});
